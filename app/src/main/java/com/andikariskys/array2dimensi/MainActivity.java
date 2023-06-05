40e32ed0c18887200c35da80a2619a093e510038
andikariskys <andikarisky313@gmail.com>
2023-06-05 23:38:35 +0700

    add list array

--- app/src/main/java/com/andikariskys/array2dimensi/MainActivity.java
+++ app/src/main/java/com/andikariskys/array2dimensi/MainActivity.java
@@ -2,7 +2,9 @@ package com.andikariskys.array2dimensi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,10 +16,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> dataMakanan = new ArrayList();
     EditText tbTambahData;
     Button btnSimpan;
     ListView listData;
+    ArrayList<String> dataMakanan = new ArrayList();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +35,21 @@ public class MainActivity extends AppCompatActivity {
         dataMakanan.add("Nasi Bakar");
         dataMakanan.add("Nasi Soto");
 
-        Adapter adapter = new ArrayAdapter(MainActivity.this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item, dataMakanan);
+        getDaftarMakanan();
 
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataMakanan.add(tbTambahData.getText().toString());
+                getDaftarMakanan();
+                tbTambahData.setText("");
+                System.out.println(tbTambahData.getText());
+            }
+        });
+    }
+
+    private void getDaftarMakanan() {
+        Adapter adapter = new ArrayAdapter(MainActivity.this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item, dataMakanan);
         listData.setAdapter((ListAdapter) adapter);
     }
 }
